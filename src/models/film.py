3e5781aa4bfb3f2be622a47f57b6fc0f66eab01d
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-03-08 14:56:02 +0300

    убраны поля из моделей, для которых нет данных на данный момент

--- src/models/film.py
+++ src/models/film.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from pydantic import Field
 from src.models.base import ModelMixin, NestedModelMixin
 
@@ -19,6 +18,3 @@ class Film(ModelMixin):
     actors: list[FilmPerson] | None = Field(default=list())
     writers: list[FilmPerson] | None = Field(default=list())
     directors: list[FilmPerson] | None = Field(default=list())
-    creation_date: datetime | None = Field(default=None)
-    age_limit: int | None = Field(default=None)
-    file_path: str | None = Field(default=None)
--- src/models/genre.py
+++ src/models/genre.py
@@ -1,8 +1,5 @@
-from pydantic import Field
-
 from src.models.base import ModelMixin
 
 
 class Genre(ModelMixin):
     name: str
-    description: str | None = Field(default="")
