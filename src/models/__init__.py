ea33d488459f40f8d7b0b47f188c0fe90e61408e
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-03-08 16:49:43 +0300

    прогнала линтеры еще раз после ребейза

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa
 from .film import *
-from .person import *
 from .genre import *
+from .person import *
--- src/models/film.py
+++ src/models/film.py
@@ -1,4 +1,5 @@
 from pydantic import Field
+
 from src.models.base import ModelMixin, NestedModelMixin
 
 
