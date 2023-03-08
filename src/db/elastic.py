4958a23f67063df486ec94f3a631747cf3f916b3
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-03-08 16:39:52 +0300

    прогнала линтеры

--- src/db/elastic.py
+++ src/db/elastic.py
@@ -1,5 +1,7 @@
 from typing import Optional
+
 from elasticsearch import AsyncElasticsearch
+
 
 es: Optional[AsyncElasticsearch] = None
 
--- src/db/redis.py
+++ src/db/redis.py
@@ -1,5 +1,7 @@
 from typing import Optional
+
 from redis.asyncio import Redis
+
 
 redis: Optional[Redis] = None
 
