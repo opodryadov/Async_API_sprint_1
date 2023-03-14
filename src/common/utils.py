75783a1c7964d3457adec1034f6be472efe33d51
Oleg Podryadov <oleg_podryadov@mail.ru>
2023-03-14 12:52:35 +0500

    Добавлены новые функции

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,9 +1,40 @@
 from fastapi import Query
 
 
+async def get_sort(field: str) -> dict:
+    if not field or field not in \
+            ("title", "-title", "imdb_rating", "-imdb_rating"):
+        return {}
+
+    method = "desc" if "-" in field else "asc"
+    field = field.replace("-", "")
+    if "title" in field:
+        field = field.replace("title", "title.raw")
+
+    return {field: method}
+
+
 async def query_params(
     query: str | None = Query(default=""),
-    page_size: int | None = Query(default=50),
     page_number: int | None = Query(default=1),
-):
-    return {"query": query, "page_size": page_size, "page_number": page_number}
+    page_size: int | None = Query(default=50),
+) -> dict:
+    return {
+        "query": query,
+        "page_number": page_number,
+        "page_size": page_size,
+    }
+
+
+async def filter_sort_query_params(
+    genre: str | None = Query(default=""),
+    sort: str | None = Query(default=""),
+    page_number: int | None = Query(default=1),
+    page_size: int | None = Query(default=50),
+) -> dict:
+    return {
+        "genre": genre,
+        "sort": await get_sort(sort),
+        "page_number": page_number,
+        "page_size": page_size,
+    }
