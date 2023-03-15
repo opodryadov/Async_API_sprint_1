265f6771b796cd0a22be51f7a2d2aa035d53fbd0
Аксентий Виктория Георгиевна <vgaksentii@domclick.ru>
2023-03-15 12:42:32 +0300

    добавила описание в ручки, убрала лишние параметры из ручки жанров

--- src/api/v1/genres.py
+++ src/api/v1/genres.py
@@ -1,9 +1,8 @@
 import logging
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 
-from src.common.utils import query_params
 from src.models import Genre
 from src.models.response import BadRequest, NotFound
 from src.services.genre import GenreService
@@ -18,6 +17,9 @@ logger = logging.getLogger(__name__)
     "/{genre_id}",
     response_model=Genre,
     responses={404: {"model": NotFound}, 400: {"model": BadRequest}},
+    summary="Получить информацию о жанре",
+    description="Получить информацию о жанре.",
+    response_description="Подробная информация о жанре.",
 )
 async def genre_details(
     genre_id: str,
@@ -33,11 +35,19 @@ async def genre_details(
     return genre
 
 
-@router.get("/", response_model=list[Genre])
+@router.get(
+    "/",
+    response_model=list[Genre],
+    summary="Список жанров",
+    description="Список жанров.",
+    response_description="Весь список жанров.",
+)
 async def list_genres(
     genre_service: GenreService = Depends(GenreService),
-    params: dict = Depends(query_params),
+    page_number: int | None = Query(default=1),
+    page_size: int | None = Query(default=50),
 ) -> list[dict]:
+    params = dict(page_number=page_number, page_size=page_size)
     genres = await genre_service.get_list_genres(params)
     if not genres:
         logger.warning("Was not a single genre")
