9cac73db34523d595ea0ee9380b3da6995425537
opodryadov <oleg_podryadov@mail.ru>
2023-03-22 16:21:20 +0500

    Реализована работа с внешними сервисами через backoff

--- src/common/connectors/elastic.py
+++ src/common/connectors/elastic.py
@@ -1,10 +1,16 @@
 from typing import Optional
 
-from elasticsearch import AsyncElasticsearch
+import backoff
+from elasticsearch import AsyncElasticsearch, ConnectionError
 
 
 es: Optional[AsyncElasticsearch] = None
 
 
+@backoff.on_exception(
+    backoff.expo,
+    (ConnectionError,),
+    max_tries=10,
+)
 async def get_elastic() -> AsyncElasticsearch:
     return es
--- src/common/connectors/redis.py
+++ src/common/connectors/redis.py
@@ -1,10 +1,16 @@
 from typing import Optional
 
-from aioredis import Redis
+import backoff
+from aioredis import Redis, ConnectionError
 
 
 redis: Optional[Redis] = None
 
 
+@backoff.on_exception(
+    backoff.expo,
+    (ConnectionError,),
+    max_tries=10,
+)
 async def get_redis() -> Redis:
     return redis
