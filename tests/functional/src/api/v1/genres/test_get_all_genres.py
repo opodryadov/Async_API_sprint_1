fcd641bc94d44b2930e9f407e90e71083fc46e37
opodryadov <oleg_podryadov@mail.ru>
2023-03-25 02:06:57 +0500

    Добавлена проверка записи в кэш Redis

--- tests/functional/src/api/v1/genres/test_get_all_genres.py
+++ tests/functional/src/api/v1/genres/test_get_all_genres.py
@@ -6,7 +6,10 @@ import pytest
 pytestmark = pytest.mark.asyncio
 
 
-async def test_get_all_genres(make_get_request):
+@pytest.mark.usefixtures("flush_redis")
+async def test_get_all_genres(make_get_request, redis_client):
+    redis_key = "bff0b4bf49e1bf3ff01d8b1fc8a11ca1"
+    params = {"page_number": 1, "page_size": 50}
     genre_list = (
         "Action", "Adventure", "Fantasy", "Sci-Fi",
         "Drama", "Music", "Romance", "Thriller", "Mystery",
@@ -16,13 +19,18 @@ async def test_get_all_genres(make_get_request):
         "Talk-Show", "News"
     )
 
-    body, status = await make_get_request(f"/api/v1/genres")
+    body, status = await make_get_request(
+        f"/api/v1/genres", params=params
+    )
     assert status == HTTPStatus.OK
 
     genre_list_from_body = [genre.get('name') for genre in body]
 
     for genre in genre_list:
         assert genre in genre_list_from_body
+
+    data = await redis_client.get(name=redis_key)
+    assert data is not None
 
 
 async def test_get_422(make_get_request):
