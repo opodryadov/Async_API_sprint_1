87e3a9eacd2843b4fa474f56da675fade4e216f3
opodryadov <oleg_podryadov@mail.ru>
2023-03-25 01:17:40 +0500

    Убран es_client

--- tests/functional/src/api/v1/films/test_get_film_by_id.py
+++ tests/functional/src/api/v1/films/test_get_film_by_id.py
@@ -12,7 +12,7 @@ unknown_film_id = "96dad231-e626-420c-b076-c71ec012ef63"
 
 
 @pytest.mark.usefixtures("flush_redis")
-async def test_get_film_by_id(make_get_request, redis_client, es_client):
+async def test_get_film_by_id(make_get_request, redis_client):
     body, status = await make_get_request(f"/api/v1/films/{film_id}")
     assert status == HTTPStatus.OK
     assert body.get("uuid") == film_id
