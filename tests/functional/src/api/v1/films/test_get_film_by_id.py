45865228e2cb1b6d63508cea4526585bbdb87990
opodryadov <oleg_podryadov@mail.ru>
2023-03-27 20:15:20 +0500

    Переписаны функциональные тесты

--- tests/functional/src/api/v1/films/test_get_film_by_id.py
+++ tests/functional/src/api/v1/films/test_get_film_by_id.py
@@ -1,43 +1,53 @@
-import json
 from http import HTTPStatus
 
+import orjson
 import pytest
+
+from tests.functional.testdata.vars.films import (
+    FILM_INFO,
+    FILM_INFO_CACHE,
+    FILM_WITHOUT_RATING_AND_PERSONS,
+    FILM_WITHOUT_RATING_AND_PERSONS_CACHE,
+)
 
 
 pytestmark = pytest.mark.asyncio
 
-film_id = "00af52ec-9345-4d66-adbe-50eb917f463a"
-film_title = "Star Slammer"
-unknown_film_id = "96dad231-e626-420c-b076-c71ec012ef63"
-
 
 @pytest.mark.usefixtures("flush_redis")
-async def test_get_film_by_id(make_get_request, redis_client):
+@pytest.mark.parametrize(
+    "film_id, api_response, redis_response",
+    (
+        (
+            "27b726f2-40e9-4d81-b608-57f7c41bfe54",
+            FILM_INFO,
+            FILM_INFO_CACHE,
+        ),
+        (
+            "32fcd689-3119-4225-8076-fdeabc553c61",
+            FILM_WITHOUT_RATING_AND_PERSONS,
+            FILM_WITHOUT_RATING_AND_PERSONS_CACHE,
+        ),
+    ),
+)
+async def test_get_film_by_id(
+    make_get_request, redis_client, film_id, api_response, redis_response
+):
     body, status = await make_get_request(f"/api/v1/films/{film_id}")
     assert status == HTTPStatus.OK
-    assert body.get("uuid") == film_id
-    assert body.get("title") == film_title
-    assert body.get("imdb_rating") == 3.5
-    assert len(body.get("genre")) == 3
-    assert len(body.get("actors")) == 4
-    assert len(body.get("writers")) == 3
-    assert len(body.get("directors")) == 1
+    assert body == api_response
 
-    genres = [genre["name"] for genre in body.get("genre")]
-    assert "Action" in genres
-
-    writers = [genre["full_name"] for genre in body.get("writers")]
-    directors = [genre["full_name"] for genre in body.get("directors")]
-    assert "Fred Olen Ray" in writers and directors
-
-    data = await redis_client.get(name=film_id)
-    assert data is not None
-
-    data = json.loads(data)
-    assert data.get("id") == film_id
-    assert data.get("title") == film_title
+    films_in_cache = await redis_client.get(film_id)
+    films_deserialize = orjson.loads(films_in_cache)
+    assert films_deserialize == redis_response
 
 
-async def test_get_404(make_get_request):
+async def test_get_film_not_found(make_get_request, redis_client):
+    unknown_film_id = "96dad231-e626-420c-b076-c71ec012ef63"
+
     body, status = await make_get_request(f"/api/v1/films/{unknown_film_id}")
     assert status == HTTPStatus.NOT_FOUND
+    assert body == {"detail": "Film not found"}
+
+    film_in_cache = await redis_client.get(unknown_film_id)
+    assert film_in_cache is None
