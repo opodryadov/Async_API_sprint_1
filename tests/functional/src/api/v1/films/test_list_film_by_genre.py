cd929fbbe553858088f520a3d0977e179c76a8d6
opodryadov <oleg_podryadov@mail.ru>
2023-03-28 16:31:12 +0500

    Отдельно вынесена сортировка

--- tests/functional/src/api/v1/films/test_list_film_by_genre.py
+++ tests/functional/src/api/v1/films/test_list_film_by_genre.py
@@ -3,73 +3,32 @@ from http import HTTPStatus
 import orjson
 import pytest
 
-from tests.functional.testdata.vars.films import (
-    GENRE_FILMS_SORT_BY_TITLE_ASC_RESPONSE,
-    GENRE_FILMS_SORT_BY_TITLE_ASC_IN_CACHE,
-    GENRE_FILMS_SORT_BY_RATING_ASC_RESPONSE,
-    GENRE_FILMS_SORT_BY_RATING_ASC_IN_CACHE,
-    GENRE_FILMS_SORT_BY_TITLE_DESC_RESPONSE,
-    GENRE_FILMS_SORT_BY_TITLE_DESC_IN_CACHE,
-    GENRE_FILMS_SORT_BY_RATING_DESC_RESPONSE,
-    GENRE_FILMS_SORT_BY_RATING_DESC_IN_CACHE,
-)
-
 
 pytestmark = pytest.mark.asyncio
 
 
 @pytest.mark.usefixtures("flush_redis")
 @pytest.mark.parametrize(
-    "genre_id, sort, redis_key, api_response, redis_response",
+    "genre_id, redis_key, api_response, redis_response",
     (
         (
-            "5373d043-3f41-4ea8-9947-4b746c601bbd",
-            "title",
-            "08dab16fecd973c507ecb2bde4f21b4c",
-            GENRE_FILMS_SORT_BY_TITLE_ASC_RESPONSE,
-            GENRE_FILMS_SORT_BY_TITLE_ASC_IN_CACHE,
-        ),
-        (
-            "31cabbb5-6389-45c6-9b48-f7f173f6c40f",
-            "imdb_rating",
-            "86b4d7f80994b6bf56ed8fa4a09dd17d",
-            GENRE_FILMS_SORT_BY_RATING_ASC_RESPONSE,
-            GENRE_FILMS_SORT_BY_RATING_ASC_IN_CACHE,
-        ),
-        (
-            "55c723c1-6d90-4a04-a44b-e9792040251a",
-            "-title",
-            "b84906f77de129d4ef369124d8b3ef81",
-            GENRE_FILMS_SORT_BY_TITLE_DESC_RESPONSE,
-            GENRE_FILMS_SORT_BY_TITLE_DESC_IN_CACHE,
-        ),
-        (
-            "f39d7b6d-aef2-40b1-aaf0-cf05e7048011",
-            "-imdb_rating",
-            "43e74e1bbeeec1d3e52ad1b3d1593e20",
-            GENRE_FILMS_SORT_BY_RATING_DESC_RESPONSE,
-            GENRE_FILMS_SORT_BY_RATING_DESC_IN_CACHE,
-        ),
-        (
             "00fcd60c-e940-4362-b30a-701f428d49a4",
-            None,
             "6ca8a3b2e1a505433787beb4faa893f0",
             [],
             [],
-        )
+        ),
     ),
 )
 async def test_list_film_by_genre(
     make_get_request,
     redis_client,
     genre_id,
-    sort,
     redis_key,
     api_response,
     redis_response,
 ):
     body, status = await make_get_request(
-        f"/api/v1/films?genre={genre_id}&sort={sort}"
+        f"/api/v1/films?genre={genre_id}"
     )
     assert status == HTTPStatus.OK
     assert body == api_response
--- tests/functional/src/api/v1/films/test_search_films.py
+++ tests/functional/src/api/v1/films/test_search_films.py
@@ -3,56 +3,16 @@ from http import HTTPStatus
 import orjson
 import pytest
 
-from tests.functional.testdata.vars.films import (
-    SEARCH_FILMS_SORT_BY_RATING_ASC_RESPONSE,
-    SEARCH_FILMS_SORT_BY_RATING_ASC_IN_CACHE,
-    SEARCH_FILMS_SORT_BY_TITLE_ASC_RESPONSE,
-    SEARCH_FILMS_SORT_BY_TITLE_ASC_IN_CACHE,
-    SEARCH_FILMS_SORT_BY_RATING_DESC_RESPONSE,
-    SEARCH_FILMS_SORT_BY_RATING_DESC_IN_CACHE,
-    SEARCH_FILMS_SORT_BY_TITLE_DESC_RESPONSE,
-    SEARCH_FILMS_SORT_BY_TITLE_DESC_IN_CACHE,
-)
-
 
 pytestmark = pytest.mark.asyncio
 
 
 @pytest.mark.usefixtures("flush_redis")
 @pytest.mark.parametrize(
-    "query, sort, redis_key, api_response, redis_response",
+    "query, redis_key, api_response, redis_response",
     (
         (
-            "luxury",
-            "imdb_rating",
-            "3b465990a7b0af63cdbaae3be7937c60",
-            SEARCH_FILMS_SORT_BY_RATING_ASC_RESPONSE,
-            SEARCH_FILMS_SORT_BY_RATING_ASC_IN_CACHE,
-        ),
-        (
-            "luxury",
-            "title",
-            "1be737ce1275cb7f88d911b858896fdb",
-            SEARCH_FILMS_SORT_BY_TITLE_ASC_RESPONSE,
-            SEARCH_FILMS_SORT_BY_TITLE_ASC_IN_CACHE,
-        ),
-        (
-            "luxury",
-            "-imdb_rating",
-            "a730349861e98b7652aadaaa47fb6d12",
-            SEARCH_FILMS_SORT_BY_RATING_DESC_RESPONSE,
-            SEARCH_FILMS_SORT_BY_RATING_DESC_IN_CACHE,
-        ),
-        (
-            "luxury",
-            "-title",
-            "0ba189f091d383f11522147cb3a1b4ba",
-            SEARCH_FILMS_SORT_BY_TITLE_DESC_RESPONSE,
-            SEARCH_FILMS_SORT_BY_TITLE_DESC_IN_CACHE,
-        ),
-        (
             "nevsky",
-            None,
             "86badab5aece293e7a5ad8025daff68e",
             [],
             [],
@@ -63,13 +23,12 @@ async def test_search_films(
     make_get_request,
     redis_client,
     query,
-    sort,
     redis_key,
     api_response,
     redis_response,
 ):
     body, status = await make_get_request(
-        f"/api/v1/films/search?query={query}&sort={sort}"
+        f"/api/v1/films/search?query={query}"
     )
     assert status == HTTPStatus.OK
     assert body == api_response
