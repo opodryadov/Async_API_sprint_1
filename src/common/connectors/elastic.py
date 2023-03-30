588f427223a7b619be0214d1581c68ea11f17727
opodryadov <oleg_podryadov@mail.ru>
2023-03-30 12:41:53 +0500

    Убрал лишние декораторы с функций

--- src/common/connectors/elastic.py
+++ src/common/connectors/elastic.py
@@ -1,27 +1,10 @@
 from typing import Optional
 
-import backoff
-from elasticsearch import (
-    AsyncElasticsearch,
-    ConnectionError,
-    ConnectionTimeout,
-)
-
-from src.common.handlers import backoff_handler
+from elasticsearch import AsyncElasticsearch
 
 
 es: Optional[AsyncElasticsearch] = None
 
 
-@backoff.on_exception(
-    backoff.expo,
-    (
-        ConnectionError,
-        ConnectionTimeout,
-    ),
-    max_tries=10,
-    max_time=60,
-    on_backoff=backoff_handler,
-)
-async def get_elastic() -> AsyncElasticsearch:
+def get_elastic() -> AsyncElasticsearch:
     return es
